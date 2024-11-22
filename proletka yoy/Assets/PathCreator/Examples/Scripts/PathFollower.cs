7b5a30a64cf5f9a09d3c7bd09d939f7361a81ad1
gigrofgod <sabenin2005@mail.ru>
2024-11-22 06:05:46 +0300

    finish

--- proletka yoy/Assets/PathCreator/Examples/Scripts/PathFollower.cs
+++ proletka yoy/Assets/PathCreator/Examples/Scripts/PathFollower.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace PathCreation.Examples
 {
@@ -8,10 +9,55 @@ namespace PathCreation.Examples
     {
         public PathCreator pathCreator;
         public EndOfPathInstruction endOfPathInstruction;
-        public float speed = 5;
-        float distanceTravelled;
+        public List<Vector2> controlSpeed;
+        public float startSpeed = 5;
+        public float distanceTravelled;
 
-        void Start() {
+        float Lerp(float firstFloat, float secondFloat, float by)
+        {
+            return firstFloat * (1 - by) + secondFloat * by;
+        }
+
+        float CalculatePercentage(float min, float max, float value)
+        {
+            if (Mathf.Approximately(max, min))
+            {
+                return 0f;
+            }
+            return (value - min) / (max - min);
+        }
+
+        float GetSpeed()
+        {
+            float speed = startSpeed;
+
+            if (controlSpeed == null || controlSpeed.Count == 0)
+            {
+                return speed;
+            }
+
+            if (distanceTravelled <= controlSpeed[0].x)
+            {
+                return controlSpeed[0].y;
+            }
+
+            Vector2 prev = controlSpeed[0];
+            for (int i = 1; i < controlSpeed.Count; i++)
+            {
+                Vector2 current = controlSpeed[i];
+                if (distanceTravelled < current.x)
+                {
+                    speed = Lerp(prev.y, current.y, CalculatePercentage(prev.x, current.x, distanceTravelled));
+                    return speed;
+                }
+                prev = current;
+            }
+
+            return controlSpeed[controlSpeed.Count - 1].y;
+        }
+
+        void Start()
+        {
             if (pathCreator != null)
             {
                 // Subscribed to the pathUpdated event so that we're notified if the path changes during the game
@@ -23,7 +69,7 @@ namespace PathCreation.Examples
         {
             if (pathCreator != null)
             {
-                distanceTravelled += speed * Time.deltaTime;
+                distanceTravelled += GetSpeed() * Time.deltaTime;
                 transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction);
                 transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled, endOfPathInstruction);
             }
@@ -31,7 +77,8 @@ namespace PathCreation.Examples
 
         // If the path changes during the game, update the distance travelled so that the follower's position on the new path
         // is as close as possible to its position on the old path
-        void OnPathChanged() {
+        void OnPathChanged()
+        {
             distanceTravelled = pathCreator.path.GetClosestDistanceAlongPath(transform.position);
         }
     }
